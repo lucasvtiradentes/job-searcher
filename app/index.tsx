5f55998d442fa8e6cf95e5e21e7e8c6cb06540d4
lucasvtiradentes <lucasvtiradentes@gmail.com>
2023-04-06 08:29:25 -0300

    :tada: init: initial commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+const Home = () =>  {
+  return (
+    <View>
+      <Text>OHome!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default Home
